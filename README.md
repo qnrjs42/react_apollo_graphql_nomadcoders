acd2a2ec2a70386a02ab542b6ff09e9165a5d818
qnrjs42 <qnrjs42@naver.com>
2020-12-22 20:02:21 +0900

    1-1. GET MOVIES Query

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ npm i apollo-boost @apollo/react-hooks graphql
 
 ### apollo-boost
 - GraphQL Yoga처럼 미리 설정해둔 패키지.
+
+
+***apollo 사용하기 전 graphQL server가 켜져있어야 함.***
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,24 @@
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
 const Home = () => {
-  return (
-    <>
-      <h1>Home</h1>
-    </>
-  )
+  const { loading, error,data } = useQuery(GET_MOVIES);
+  console.log(loading, error, data);
+
+  if(loading) {
+    return "loading..."
+  } 
+  if(data && data.movies) {
+    return data.movies.map((movie) => <h1>{movie.id}</h1>)
+  }
 };
 export default Home;
